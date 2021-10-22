5f6e71414826c38477705b2cbb94bd77acbe576c
sjr2021 <92972870+sjr2021@users.noreply.github.com>
2021-10-22 12:41:49 +0530

    Create Display.py

---
+++ Display.py
@@ -0,0 +1,9 @@
+list_1 = [10,20,30,40,50]
+res_list = [x^2 for x in list_1]
+print(re_list)
+
+list_2 = [101,202,303,404,505]
+sub_list = [x*0.1 for x in list_2]
+print(sub_list)
+
+print("....The list is completed.....")
